bacf1227a1f073f26b5df5580472730545376150
Leonardo Yago Nascimento Silva <71148287+L30k@users.noreply.github.com>
2023-09-03 19:54:22 -0300

    Update equalizaçãoDeHistograma.py

--- equalizaçãoDeHistograma.py
+++ equalizaçãoDeHistograma.py
@@ -1,24 +1,29 @@
-# Importa as bibliotecas necessárias
-import cv2  # OpenCV para manipulação de imagem
-import matplotlib.pyplot as plt  # Matplotlib para exibição de gráficos
-import math  # Biblioteca Math para operações matemáticas
+import cv2
+import matplotlib.pyplot as plt
+import math
 
 # Carrega a imagem de entrada
 input_image = cv2.imread('/content/photo2.jpg')
 
+# Define o tamanho da imagem
+tamanho_imagem = input_image.shape[:2]
+
+# Obtém as dimensões da imagem (altura e largura)
+altura, largura = tamanho_imagem
+
 # Inicializa três listas para processamento do histograma
-cores = [0] * 256  # Lista para contar a ocorrência de cada intensidade de cinza
-coresNovas = [0] * 256  # Lista para armazenar as novas intensidades após a equalização
-aux = [0] * 256  # Lista auxiliar para mapear as intensidades de cinza
+cores = [0] * 256
+coresNovas = [0] * 256 
+aux = [0] * 256 
 
 # Inicializa a lista auxiliar para mapear intensidades de cinza de 0 a 255
 for i in range(0, 256):
     aux[i] = i
 
 # Loop aninhado para calcular o histograma da imagem original
-for i in range(0, 492):
-    for j in range(0, 492):
-        (b, g, r) = imput_image[j, i]
+for i in range(0, largura):
+    for j in range(0, altura):
+        (b, g, r) = input_image[j, i]
         cores[b] = cores[b] + 1
 
 # Calcula o número de tons de cinza diferentes na imagem
@@ -30,7 +35,7 @@ for i in range(0, 256):
 print(gDeCinza)
 
 # Calcula a intensidade média de cinza e ajusta as novas intensidades de cinza
-iDeCinza = (492 * 492) / gDeCinza
+iDeCinza = (altura * largura) / gDeCinza
 auxCor = 0
 for i in range(0, 256):
     if cores[i] > 0:
@@ -39,11 +44,11 @@ for i in range(0, 256):
             coresNovas[i] = (auxCor // iDeCinza) - 1
 
 # Atualiza a imagem original com as novas intensidades de cinza
-for i in range(0, 492):
-    for j in range(0, 492):
-        (b, g, r) = imput_image[j, i]
+for i in range(0, largura):
+    for j in range(0, altura):
+        (b, g, r) = input_image[j, i]
         if b > 0:
-            imput_image[j, i] = (coresNovas[b], coresNovas[b], coresNovas[b])
+            input_image[j, i] = (coresNovas[b], coresNovas[b], coresNovas[b])
 
 # Exibe o histograma da imagem original
 plt.bar(aux, cores)
@@ -52,9 +57,9 @@ plt.show()
 
 # Calcula o histograma da imagem após a equalização e o exibe
 instoNovo = [0] * 256
-for i in range(0, 492):
-    for j in range(0, 492):
-        (b, g, r) = imput_image[j, i]
+for i in range(0, largura):
+    for j in range(0, altura):
+        (b, g, r) = input_image[j, i]
         instoNovo[b] = instoNovo[b] + 1
 
 plt.bar(aux, instoNovo)
@@ -62,4 +67,4 @@ plt.suptitle("Nova")
 plt.show()
 
 # Exibe a imagem após a equalização
-plt.imshow(imput_image)
+plt.imshow(input_image)
